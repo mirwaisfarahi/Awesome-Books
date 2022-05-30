1229faefbbf9b9e8e0956ae5fd5405606b6df2ab
Mirwais Farahi <waisfarahi@gmail.com>
2022-05-30 20:00:18 +0500

    Edit index.js File

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Book Class to initiate a book object for the book details
+class Book {
+    constructor(title, author) {
+        this.author = title;
+        this.author = author;
+    }
+}
+
+// Operations Class will be used to add and remove book
+class Operations {
+    // An array for the collection of books to display
+    display() {
+        const books = [
+            {
+                title: 'Introduction to Javascript',
+                author: 'A'
+            },
+            {
+                title: 'Advanced Javascript',
+                author: 'B'
+            }
+        ];
+    }
+    
+}
