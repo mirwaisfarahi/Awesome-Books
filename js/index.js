5b023beccc87b4d67543b29eba07837a44ea9618
Mirwais Farahi <waisfarahi@gmail.com>
2022-06-01 00:54:02 +0500

    Edit index.js File

--- js/index.js
+++ js/index.js
@@ -1,58 +1,57 @@
+// get local storage data
+const getData = () => JSON.parse(localStorage.getItem('books'));
 
-  // get local storage data
-  const getData = () => JSON.parse(localStorage.getItem('books'));
-  
-  // set local storage data
-  const setData = (books) => localStorage.setItem('books', JSON.stringify(books));
+// set local storage data
+const setData = (books) => localStorage.setItem('books', JSON.stringify(books));
 
-  // reload the page
-  const reloadPage = function() {
-    window.location.reload();
-    return false;
-  }
+// reload the page
+const reloadPage = function () {
+  window.location.reload();
+  return false;
+};
 
-  // Remove book
-  const remove = function(index) {
-    const books = getData();
-    books.splice(index, 1);
-    setData(books);
-    reloadPage();
-  }
+// Remove book
+const remove = function (index) {
+  const books = getData();
+  books.splice(index, 1);
+  setData(books);
+  reloadPage();
+};
 
-  // display book
-  const display = function() {
-    const books = getData();
-    const bookList = document.querySelector('#booksList');
+// display book
+const display = function () {
+  const books = getData();
+  const bookList = document.querySelector('#booksList');
 
-    for (let book = 0; book < books.length; book += 1) {
-      const tr = document.createElement('tr');
+  for (let book = 0; book < books.length; book += 1) {
+    const tr = document.createElement('tr');
 
-      tr.innerHTML = `<td>${books[book].title} by ${books[book].author}</td>
+    tr.innerHTML = `<td>${books[book].title} by ${books[book].author}</td>
                 <td><button class = "remove-book">Delete</a></td>
                 `;
-      bookList.appendChild(tr);
-    }
+    bookList.appendChild(tr);
+  }
 
-    const removeBook = document.querySelectorAll('.remove-book');
-    removeBook.forEach((btn, index) => {
-      btn.addEventListener('click', () => {
-        remove(index);
-      });
+  const removeBook = document.querySelectorAll('.remove-book');
+  removeBook.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      remove(index);
     });
-  }
+  });
+};
 
-  // Add Book
-  const add = function(bookTitle, bookAuthor) {
-    let existingBooks = getData();
-    if (existingBooks == null) existingBooks = [];
+// Add Book
+const add = function (bookTitle, bookAuthor) {
+  let existingBooks = getData();
+  if (existingBooks == null) existingBooks = [];
 
-    const book = { title: bookTitle, author: bookAuthor };
+  const book = { title: bookTitle, author: bookAuthor };
 
-    // Save allBooks back to local storage
-    existingBooks.push(book);
-    setData(existingBooks);
-    reloadPage();
-  }
+  // Save allBooks back to local storage
+  existingBooks.push(book);
+  setData(existingBooks);
+  reloadPage();
+};
 
 // Event to display books
 document.addEventListener('Content', display());
